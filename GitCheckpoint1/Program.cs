ba260981df675568f47782c40f7161af594e3dfe
Zakk Elfving <Zakk.elfving@gmail.com>
2023-02-16 14:03:08 +0100

    Add project files.

---
+++ GitCheckpoint1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace GitCheckpoint1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the Git checkpoint task 1.1");
+        }
+    }
+}
