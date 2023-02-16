facb273265e9a71a5479dbbc689482c00a7df37a
Zakk Elfving <Zakk.elfving@gmail.com>
2023-02-16 14:12:38 +0100

    calculate add implemented

---
+++ GitCheckpoint1/Calculate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCheckpoint1
+{
+    internal static class Calculate
+    {
+        public static void Add(int numOne, int numTwo)
+        {
+            Console.WriteLine($"{numOne+numTwo}");
+        }
+    }
+}
